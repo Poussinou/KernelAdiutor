86d70b3fe8f906308fe8d3191fcbb50f3289be54
NHellFire <nathan.renniewaldock@gmail.com>
2017-05-28 21:04:42 +0100

    Remove crashlytics

--- app/src/main/java/com/grarak/kerneladiutor/fragments/other/AboutFragment.java
+++ app/src/main/java/com/grarak/kerneladiutor/fragments/other/AboutFragment.java
@@ -57,7 +57,6 @@ public class AboutFragment extends RecyclerViewFragment {
         sLibraries.put("Lopez Mikhael,CircularImageView", "https://github.com/lopspower/CircularImageView");
         sLibraries.put("Square,picasso", "https://github.com/square/picasso");
         sLibraries.put("CyanogenMod,CyanogenMod Platform SDK", "https://github.com/CyanogenMod/cm_platform_sdk");
-        sLibraries.put("Fabric,Twitter", "https://get.fabric.io");
     }
 
     @Override
